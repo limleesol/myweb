d30f8b84d22a4aed818bc440d255ff6a0a69d9fa
limleesol <53771017+limleesol@users.noreply.github.com>
2020-08-11 21:00:33 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {button, progress} from 'semantic-ui-react'
 
 function App() {
   return (
+    <div>
     <p> 안녕하세요! </p>
   );
 }
