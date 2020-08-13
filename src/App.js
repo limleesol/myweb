cd9db327dd4d27fc441425f4c16c33dd0c4247c5
limleesol <53771017+limleesol@users.noreply.github.com>
2020-08-13 20:05:51 +0900

    2days

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {button, progress} from 'semantic-ui-react'
+import {Button, Input} from 'semantic-ui-react';
+
+function Login2(){
+
+  return(
+      <div>
+      <Input/>
+      <Button onClick = { (e) => {
+        console.log(e)
+        alert(e.target.value)
+      } }> 로그인! </Button>
+      </div>
+
+  )
+}
+
+class Login extends React.Component{
+  constructor(){
+    super()
+    this.state = {id : "아무것도 입력하지 않았습니다."}
+  }
+
+    render(){
+      return(
+        <div>
+        <Input onChange = { (e) => this.setState({id : e.target.value })  }/>
+        <Button value = {this.state.id} onClick = { (event) => {
+          alert(this.state.id)
+        } }> Login </Button>
+        </div>
+
+      )
+    }
+}
 
 function App() {
   return (
     <div>
-    <p> 안녕하세요! </p>
+<p> 안녕하세요! </p>
+<Button> Click </Button>
+<button> Click2 </button>
+<Login / >
+</div>
   );
 }
 
